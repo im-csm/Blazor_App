614cbe466a0108fdcd894b3691947307e0ba8948
Chance Mullen <csmullen8509@eagle.fgcu.edu>
2021-10-05 21:51:52 -0400

    Added comments explaining functionality for the Todo list

--- Client/TodoItem.cs
+++ Client/TodoItem.cs
@@ -1,8 +1,9 @@
 namespace BlazorApp.Client
 {
+    <!-- This class is used to make each item in the todo list-->
     public class TodoItem
     {
-        public string Title { get; set; }
-        public bool IsDone { get; set; }
+        public string Title { get; set; }   <!-- This will set the title of the item in the list-->
+        public bool IsDone { get; set; }    <!-- This will allow the user to check off items in the list as 'done' -->
     }
 }
